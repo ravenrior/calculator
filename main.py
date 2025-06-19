c49517d38b229216d3064921ed2315fa8e258eaf
Valerian <134371784+ravenrior@users.noreply.github.com>
2025-06-19 15:30:57 +0300

    Update main.py
    add cmd for buttons

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ root.title("Калькулятор")
 root["bg"] = 'white'
 root.geometry('280x490')
 root.resizable(False, False)
-
 root.bind('q', quit)
 
+def on_click(sym):
+    print(sym)
+
 def create_buttons(button_dict, y, root: type[Tk] | None = root):
-    return [
-        My_Button(symbol, x, y, root).create_button()
-        for x, symbol in button_dict.items()
-    ]
+    return [My_Button(symbol, x, y, root).create_button(on_click) for x, symbol in button_dict.items()]
 
 dict_row_420 = {210: '=', 140: ',', 70: '00', 0: '0'}
 dict_row_350 = {210: '+', 140: '3', 70: '2', 0: '1'}
@@ -23,15 +22,6 @@ dict_row_140 = {210: '/', 140: '%', 70: '()', 0: 'C'}
 
 for i in range(1, 6):
     create_buttons(globals()[f'dict_row_{(i+1)*70}'], (i+1)*70)
-
-
-
-
-
-
-
-
-
 
 
 #  C ()  %  /
